fbb4370deea01812c7c801aa9e777739b238789c
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2024-07-06 16:49:41 +0300

    Отредактировал второй слайд

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import Slide2 from "./components/Slide2/Slide2";
-import Slider from "./components/Slider/Slider";
 import SlideOne from "./components/SlideOne/SlideOne";
+import SlideTwo from "./components/SlideTwo/SlideTwo";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SlideOne />
+      {/* <SlideOne /> */}
+      <SlideTwo />
       {/* <Slider>
 	  <SlideOne />
         <Slide2 />
---
+++ src/components/SlideTwo/SlideTwo.jsx
@@ -0,0 +1,74 @@
+import "./SlideTwo.css";
+
+export default function SlideTwo() {
+  return (
+    <section className="slide slide_second">
+      <div className="slide__container slide__container_second">
+        <p className="slide__title">
+          ТЕКСТ <br /> СООБЩЕНИЯ
+        </p>
+        <div className="scroll">
+          <div className="text__container">
+            <p className="text">
+              <b>Lorem ipsum dolor sit amet</b>, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Duis ut diam quam nulla. Mauris in aliquam sem fringilla ut morbi
+              tincidunt. Vitae aliquet nec ullamcorper sit amet risus nullam
+              eget felis. Nulla pharetra diam sit amet nisl. Eget nulla facilisi
+              etiam dignissim diam quis enim lobortis. Est sit amet facilisis
+              magna. Neque laoreet suspendisse interdum consectetur libero id.
+              Nec ullamcorper sit amet risus nullam eget felis eget. Mollis
+              aliquam ut porttitor leo a diam sollicitudin tempor id. Euismod
+              quis viverra nibh cras pulvinar mattis nunc. Massa massa ultricies
+              mi quis. Sit amet massa vitae tortor condimentum lacinia. Et
+              malesuada fames ac turpis egestas integer eget. Elementum pulvinar
+              etiam non quam lacus suspendisse faucibus interdum posuere. Amet
+              justo donec enim diam vulputate ut pharetra sit. Risus ultricies
+              tristique nulla aliquet enim tortor at auctor. Velit sed
+              ullamcorper morbi tincidunt ornare massa. Quis hendrerit dolor
+              magna eget est lorem ipsum. Etiam dignissim diam quis enim.
+              Gravida neque convallis a cras. Ut enim blandit volutpat maecenas
+              volutpat. Mauris sit amet massa vitae tortor condimentum lacinia
+              quis vel. Mauris in aliquam sem fringilla ut morbi tincidunt.
+              Vitae aliquet nec ullamcorper sit amet risus nullam eget felis.
+              Nulla pharetra diam sit amet nisl. Eget nulla facilisi etiam
+              dignissim diam quis enim lobortis. Est sit amet facilisis magna.
+              Neque laoreet suspendisse interdum consectetur libero id. Nec
+              ullamcorper sit amet risus nullam eget felis eget. Mollis aliquam
+              ut porttitor leo a diam sollicitudin tempor id. Euismod quis
+              viverra nibh cras pulvinar mattis nunc. Massa massa ultricies mi
+              quis. Sit amet massa vitae tortor condimentum lacinia. Et
+              malesuada fames ac turpis egestas integer eget. Elementum pulvinar
+              etiam non quam lacus suspendisse faucibus interdum posuere. Amet
+              justo donec enim diam vulputate ut pharetra sit. Risus ultricies
+              tristique nulla aliquet enim tortor at auctor. Velit sed
+              ullamcorper morbi tincidunt ornare massa. Quis hendrerit dolor
+              magna eget est lorem ipsum. Etiam dignissim diam quis enim.
+              Gravida neque convallis a cras. Ut enim blandit volutpat maecenas
+              volutpat. Mauris sit amet massa vitae tortor condimentum lacinia
+              quis vel. Mauris in aliquam sem fringilla ut morbi tincidunt.
+              Vitae aliquet nec ullamcorper sit amet risus nullam eget felis.
+              Nulla pharetra diam sit amet nisl. Eget nulla facilisi etiam
+              dignissim diam quis enim lobortis. Est sit amet facilisis magna.
+              Neque laoreet suspendisse interdum consectetur libero id. Nec
+              ullamcorper sit amet risus nullam eget felis eget. Mollis aliquam
+              ut porttitor leo a diam sollicitudin tempor id. Euismod quis
+              viverra nibh cras pulvinar mattis nunc. Massa massa ultricies mi
+              quis. Sit amet massa vitae tortor condimentum lacinia. Et
+              malesuada fames ac turpis egestas integer eget. Elementum pulvinar
+              etiam non quam lacus suspendisse faucibus interdum posuere. Amet
+              justo donec enim diam vulputate ut pharetra sit. Risus ultricies
+              tristique nulla aliquet enim tortor at auctor. Velit sed
+              ullamcorper morbi tincidunt ornare massa. Quis hendrerit dolor
+              magna eget est lorem ipsum. Etiam dignissim diam quis enim.
+              Gravida neque convallis a cras. Ut enim blandit volutpat maecenas
+              volutpat. Mauris sit amet massa vitae tortor condimentum lacinia
+              quis vel.
+            </p>
+          </div>
+          <div className="test2"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
